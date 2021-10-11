3354f46c7406ad8c3b0a8222d79683b19c8b155c
henry-the-vietnamese <henryfromvietnam@gmail.com>
2021-10-11 18:24:18 +1030

    corrupt the program as soon as the user enters an invalid duration

--- salary_calculator.py
+++ salary_calculator.py
@@ -16,18 +16,20 @@ HOURS_PER_DAY = int(input('How many hours do you work per day: '))
 duration = input('Enter the duration to calculate (day/week/month/year): ')
 
 # Duration input validation.
-while duration not in ['day', 'week', 'month', 'year']:
+if duration not in ['day', 'week', 'month', 'year']:
     print('Error: Invalid duration')
-    duration = input('Enter the duration to calculate (day/week/month/year): ')
-
-# Calculate the salary based on user input duration.
-if duration == 'day':
-    print(f'After a day you will earn ${SALARY_PER_HOUR * HOURS_PER_DAY}')
 else:
-    DAYS_PER_WEEK = int(input('How many days do you work per week: '))
-    if duration == 'week':
-        print(f'After a {duration} you will earn ${SALARY_PER_HOUR * HOURS_PER_DAY * DAYS_PER_WEEK}')
-    elif duration == 'month':
-        print(f'After a {duration} you will earn between ${SALARY_PER_HOUR * HOURS_PER_DAY * DAYS_PER_WEEK * 4} and ${round(SALARY_PER_HOUR * HOURS_PER_DAY * DAYS_PER_WEEK * 4.34524, 2)}')
+    """
+    The duration is valid: belongs to one of the four - day/week/month/year.
+    Now calculate the salary based on the user input duration.
+    """
+    if duration == 'day':
+        print(f'After a day you will earn ${SALARY_PER_HOUR * HOURS_PER_DAY}')
     else:
-        print(f'After a {duration} you will earn between ${SALARY_PER_HOUR * HOURS_PER_DAY * DAYS_PER_WEEK * 48} and ${round(SALARY_PER_HOUR * HOURS_PER_DAY * DAYS_PER_WEEK * 52.1329, 2)}')
+        DAYS_PER_WEEK = int(input('How many days do you work per week: '))
+        if duration == 'week':
+            print(f'After a {duration} you will earn ${SALARY_PER_HOUR * HOURS_PER_DAY * DAYS_PER_WEEK}')
+        elif duration == 'month':
+            print(f'After a {duration} you will earn between ${SALARY_PER_HOUR * HOURS_PER_DAY * DAYS_PER_WEEK * 4} and ${round(SALARY_PER_HOUR * HOURS_PER_DAY * DAYS_PER_WEEK * 4.34524, 2)}')
+        else:
+            print(f'After a {duration} you will earn between ${SALARY_PER_HOUR * HOURS_PER_DAY * DAYS_PER_WEEK * 48} and ${round(SALARY_PER_HOUR * HOURS_PER_DAY * DAYS_PER_WEEK * 52.1329, 2)}')

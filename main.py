e2608486810cb3b4a257675f355d76aa56c62b57
Henry Mai <henryfromvietnam@gmail.com>
2023-07-13 03:13:33 +0700

    fix: flake8 suggestions

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
+# =============================================================================
 #
-# File:         salary_calculator.py
-# Author:       Tan Duc Mai
-# Email:        tan.duc.work@gmail.com
-# Date:         11-Oct-21
-# Description:  Calculate how much a user will get paid after a day, week,
+#        FILE:  main.py
+#      AUTHOR:  Tan Duc Mai <henryfromvietnam@gmail.com>
+#     CREATED:  2021-10-11
+# DESCRIPTION:  Calculate how much a user will get paid after a day, week,
 #               month, or year.
 #   I hereby declare that I completed this work without any improper help
 #   from a third party and without using any aids other than those cited.
 #
-
+# =============================================================================
 
 # Variable initialisation.
 salary_per_hour = float(input('How much do you get paid per hour: '))
 
+# Hour input validation.
 hours_per_day = float(input('How many hours do you work per day: '))
-while not 0 <= hours_per_day <= 24:             # Hour input validation.
+while not 0 <= hours_per_day <= 24:
     print('ERROR: Invalid hour value')
     hours_per_day = float(input('How many hours do you work per day: '))
 
+# Duration input validation.
 duration = input('Enter the duration to calculate (day/week/month/year): ')
-while duration not in ['day', 'week',
-                       'month', 'year']:        # Duration input validation.
+while duration not in ['day', 'week', 'month', 'year']:
     print('ERROR: Invalid duration')
     duration = input('Enter the duration to calculate (day/week/month/year): ')
 
@@ -50,10 +52,10 @@ else:
 
     elif duration == 'month':
         print(
-            f'After a {duration} you will earn between ${long_term_salary * 4}'
+            f'After a {duration} you will earn between ${long_term_salary*4}'
             f' and ${long_term_salary * 4.34524 : .2f}')
 
     else:
         print(
-            f'After a {duration} you will earn between ${long_term_salary * 48}'
+            f'After a {duration} you will earn between ${long_term_salary*48}'
             f' and ${long_term_salary * 52.1329 : .2f}')
